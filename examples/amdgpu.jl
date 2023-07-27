2d90a7861074997df65fe2764ae7abcc7edaa9e8
Michael F. Herbst <info@michael-herbst.com>
2023-07-27 16:27:39 +0200

    up examples

--- examples/amdgpu.jl
+++ examples/amdgpu.jl
@@ -8,7 +8,36 @@ lattice = a / 2 * [[0 1 1.];
 Si = ElementPsp(:Si, psp=load_psp("hgh/lda/Si-q4"))
 atoms     = [Si, Si]
 positions = [ones(3)/8, -ones(3)/8]
-model = model_PBE(lattice, atoms, positions)
 
-basis  = PlaneWaveBasis(model; Ecut=30, kgrid=(5, 5, 5), architecture=DFTK.GPU(AMDGPU.ROCArray)
-scfres = self_consistent_field(basis; tol=1e-2, solver=scf_damping_solver())
+architecture = DFTK.GPU(AMDGPU.ROCArray)
+
+if false
+    # Full problem
+    model  = model_PBE(lattice, atoms, positions)
+    basis  = PlaneWaveBasis(model; Ecut=20, kgrid=(3, 3, 3), architecture)
+    scfres = self_consistent_field(basis; tol=1e-2, solver=scf_damping_solver())
+elseif false
+    # Slightly reduced problem
+    model = model_atomic(lattice, atoms, positions)
+    basis = PlaneWaveBasis(model; Ecut=20, kgrid=(1, 1, 1), architecture)
+else
+    # Simplest failing problem
+    model = Model(lattice, atoms, positions)
+    basis = PlaneWaveBasis(model; Ecut=20, kgrid=(1, 1, 1), architecture)
+end
+
+println("basis done")
+psi = DFTK.random_orbitals(basis, basis.kpoints[1], 6)
+@show typeof(psi)
+println("psi done")
+
+ham = Hamiltonian(basis)
+println("ham done")
+
+res = ham * psi
+println("apply done")
+
+if false  # Do full eigensolve
+    eigres = diagonalize_all_kblocks(DFTK.lobpcg_hyper, ham, 6);
+    println("eigensolve done")
+end
